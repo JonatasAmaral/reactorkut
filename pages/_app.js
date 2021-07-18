42281b9d7456826488ff29ce62482b5fd9c37700
Jonatas Amaral <jonataspadtab@gmail.com>
2021-07-18 19:21:10 -0300

    make page's grid layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,36 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-	body {
+	* {
 		margin: 0;
 		padding: 0;
 		box-sizing: border-box;
 	}
-`
+	body {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		background-color: #D9E6F6;
+	}
+
+	#__next{
+		min-height: 100vh;
+		display: flex;
+		flex-direction: column
+	}
+
+	img {
+		max-width: 100%;
+		height: auto;
+		display: block;
+	}
+`;
 
 const theme = {
 	colors: {
-		primary: '#0070f3',
+		primary: "#0070f3",
 	},
-}
+};
 
 export default function App({ Component, pageProps }) {
 	return (
@@ -22,5 +40,5 @@ export default function App({ Component, pageProps }) {
 				<Component {...pageProps} />
 			</ThemeProvider>
 		</>
-	)
+	);
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,41 @@
-import styled from 'styled-components'
+import MainGrid from "../src/components/MainGrid";
+import Box from "../src/components/Box";
 
-const Title = styled.h1`
-	font-size: 50px;
-	color: ${({ theme }) => theme.colors.primary};
-`
+// const Title = styled.h1`
+//	font-size: 50px;
+//	color: ${({ theme }) => theme.colors.primary};
+// `
+
+function ProfileSidebar({githubUser}) {
+	return (
+		<Box>
+			<img
+				style={{borderRadius: '8px'}}
+				src={`https://github.com/${githubUser}.png`}
+			/>
+		</Box>
+	)
+}
 
 export default function Home() {
-	return <Title>My page</Title>
+	const githubUser = 'JonatasAmaral';
+	return (
+		<MainGrid>
+			{/* <Box style="grid-area: profileArea">Image</Box> */}
+			<div className="profileArea" style={{gridArea: 'profileArea'}}>
+				<ProfileSidebar {...{githubUser}} />
+				<Box>User info</Box>
+				<Box>Menu</Box>
+			</div>
+			<div className="welcomeArea" style={{gridArea: 'welcomeArea'}}>
+				<Box>Bem vindo</Box>
+				<Box>Postar</Box>
+				<Box>Feed</Box>
+			</div>
+			<div className="profileRelationsArea" style={{gridArea: 'profileRelationsArea'}}>
+				<Box>Amigos</Box>
+				<Box>Comunidades</Box>
+			</div>
+		</MainGrid>
+	)
 }
